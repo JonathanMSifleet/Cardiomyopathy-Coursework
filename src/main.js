9ccba120284db43814b5756a838b2e5e9658577b
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-01 22:51:07 +0000

    update skeleton code
    set up axios and firebase for later use

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import axios from "axios";
+import firebase from "firebase/app";
+//will do firebase initialization later.
 import "./assets/styles/main.css";
-
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(axios);
 app.mount("#app");
