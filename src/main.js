3b132c13570d81f89a71eeaf6b34160645695253
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-01 22:51:51 +0000

    Update main.js
    comment to install axios and firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//npm i axios firebase
 import axios from "axios";
 import firebase from "firebase/app";
 //will do firebase initialization later.
