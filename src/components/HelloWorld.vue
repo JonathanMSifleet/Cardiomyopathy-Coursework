efd076680e469a3930d4c3412a23ee225c658612
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-02 12:49:30 +0000

    Add linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
+      >vue-cli documentation</a
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
@@ -15,7 +15,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
+        >babel</a
         >
       </li>
       <li>
@@ -23,7 +23,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
-          >router</a
+        >router</a
         >
       </li>
       <li>
@@ -31,7 +31,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
           target="_blank"
           rel="noopener"
-          >vuex</a
+        >vuex</a
         >
       </li>
       <li>
@@ -39,7 +39,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-          >eslint</a
+        >eslint</a
         >
       </li>
     </ul>
@@ -50,17 +50,17 @@
       </li>
       <li>
         <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
+        >Forum</a
         >
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
+        >Community Chat</a
         >
       </li>
       <li>
         <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
+        >Twitter</a
         >
       </li>
       <li>
@@ -71,7 +71,7 @@
     <ul>
       <li>
         <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
+        >vue-router</a
         >
       </li>
       <li>
@@ -82,12 +82,12 @@
           href="https://github.com/vuejs/vue-devtools#vue-devtools"
           target="_blank"
           rel="noopener"
-          >vue-devtools</a
+        >vue-devtools</a
         >
       </li>
       <li>
         <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
+        >vue-loader</a
         >
       </li>
       <li>
@@ -95,7 +95,7 @@
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
           rel="noopener"
-          >awesome-vue</a
+        >awesome-vue</a
         >
       </li>
     </ul>
@@ -103,28 +103,15 @@
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  @import './HelloWorld.module.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-//npm i axios firebase
-import axios from "axios";
-import firebase from "firebase/app";
-//will do firebase initialization later.
-import "./assets/styles/main.css";
+import { createApp } from 'vue';
+// import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './assets/styles/main.module.css';
+
+// npm i axios firebase
+// will do firebase initialization later.
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.use(axios);
-app.mount("#app");
+// app.use(axios);
+app.mount('#app');
