80a5de1696475bf20da321cfdfc297a0273a5a1c
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-02 12:54:23 +0000

    Fix loading css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './assets/styles/main.css';
+import './assets/styles/main.module.css';
 // import axios from "axios";
 // import firebase from "firebase/app";
 
