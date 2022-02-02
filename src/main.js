a1b3f2b28d110bc2fe8b587463f00e92e397a920
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-02 12:13:32 +0000

    unused import and .env file
    i have made the .env file for now to make it easier for everyone to add and will be updating the .ignore file a bit later when everyone has got everything set up

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 //npm i axios firebase
-import axios from "axios";
-import firebase from "firebase/app";
+// import axios from "axios";
+// import firebase from "firebase/app";
 //will do firebase initialization later.
 import "./assets/styles/main.css";
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.use(axios);
+// app.use(axios);
 app.mount("#app");
