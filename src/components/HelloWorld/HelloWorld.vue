c0afa070374ac712d17f5aa025fc2818dd50ae69
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-05 00:00:43 +0000

    Folder restructuring

--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -112,6 +112,6 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  @import './HelloWorld.module.css';
+<style scoped lang="scss">
+  @import './HelloWorld.module.scss';
 </style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <PageWrapper>
+    <HelloWorld msg="Welcome to My Vue.js App" />
+  </PageWrapper>
+</template>
+
+<script>
+  import HelloWorld from '../../components/HelloWorld/HelloWorld.vue';
+  import PageWrapper from '../../hoc/PageWrapper/PageWrapper.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld, PageWrapper
+    }
+  };
+</script>
