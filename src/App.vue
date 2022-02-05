8754b77f71e86a61904ddcaed3f7907bcd842d49
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-05 00:03:23 +0000

    Hide links on page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/"/>
+    <router-link to="/about"/>
   </div>
   <router-view />
 </template>
-
-<style></style>
