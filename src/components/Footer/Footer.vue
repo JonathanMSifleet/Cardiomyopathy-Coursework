392e7517b91bcada74a65bf042353f016a62bf66
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-05 11:56:56 +0000

    Create footer and header components

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <MDBFooter bg="success" class="fixed-bottom" :text="['center', 'white', 'lg-start']">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      <a class="text-dark" href="https://github.com/MuminRahman2001/advanced-web-project">
+        Advanced Web Development Project
+      </a>
+    </div>
+  </MDBFooter>
+</template>
+
+<script>
+  import { MDBFooter } from 'mdb-vue-ui-kit';
+  export default {
+    components: {
+      MDBFooter
+    }
+  };
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <MDBNavbar :class="[$style.Header]" class="header" expand="lg" light
+               bg="success" container position="sticky">
+      <MDBNavbarToggler target="#navbarExample01"/>
+      <MDBNavbarNav collapse="navbarExample01" class="mb-2 mb-lg-0">
+        <MDBNavbarItem href="/" active>
+          Home
+        </MDBNavbarItem>
+        <MDBNavbarItem href="/about">
+          About
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBNavbar>
+  </header>
+</template>
+
+<script>
+  import { MDBNavbar, MDBNavbarToggler, MDBNavbarNav, MDBNavbarItem } from 'mdb-vue-ui-kit';
+
+  export default {
+    components: {
+      MDBNavbar,
+      MDBNavbarToggler,
+      MDBNavbarNav,
+      MDBNavbarItem
+    }
+  };
+</script>
+
+<style lang="scss" module scoped>
+  @import './Header.module.scss';
+</style>
