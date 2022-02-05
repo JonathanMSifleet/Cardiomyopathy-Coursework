9148fc12f0803a47a6ed86d7df3dc849e0a38c9c
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-05 11:52:05 +0000

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new VueLoaderPlugin()
+  ]
+};
