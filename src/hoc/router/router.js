2b32675d4833fc9ed19be8c11f995da0efc62bc7
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-06 00:35:40 +0000

    Update router.js
    keep home as a constant build so home is never having to be generated but make all others load on response

--- src/hoc/router/router.js
+++ src/hoc/router/router.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../../views/Home/Home.vue";
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../../views/Home/Home.vue')
-  }
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
