971a01e3138116a8d2e879877d4f798090062d14
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-06 17:38:08 +0000

    Update router.js

--- src/hoc/router/router.js
+++ src/hoc/router/router.js
@@ -11,6 +11,11 @@ const routes = [
     name: "About",
     component: () => import("../../views/About/About.vue"),
   },
+  // no route, route:
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  },
 ];
 
 const router = createRouter({
