9b94ed357476e1daacb647308d552cce4737d545
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-07 18:08:04 +0000

    moved firebase api key etc to a .env so its not public meaning its more secure

--- src/constants/firebaseDetails.js
+++ src/constants/firebaseDetails.js
@@ -1,8 +1,9 @@
+import data from './.env';
 exports.details = {
-  apiKey: 'AIzaSyBuiC69m_Rz3X4Htt4epBFqAwfq5VKULS8',
-  appId: '1:385905656452:web:ae167cdb7fb0d973feb8d1',
-  authDomain: 'frdtest-ee098.firebaseapp.com',
-  messagingSenderId: '385905656452',
-  projectId: 'frdtest-ee098',
-  storageBucket: 'frdtest-ee098.appspot.com'
+  apiKey: data.apiKey,
+  appId: data.appId,
+  authDomain: data.authDomain,
+  messagingSenderId: data.messagingSenderId,
+  projectId: data.projectId,
+  storageBucket: data.storageBucket
 };
