df86bba9957c8d837232ff8d7579fca96e326cac
Humzah99 <humzah99@live.co.uk>
2022-02-07 23:39:50 +0000

    Adding invalid credentials validation messages for the login section

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,6 +6,7 @@
       <input type="text" placeholder="Email" v-model="email" />
       <input type="password" placeholder="Password" v-model="password" />
       <input type="submit" value="Login" />
+      <p v-if="errorMessage">{{ errorMessage }}</p>
       <p><router-link to="/">Forgotten password?</router-link></p>
       <p>Need an account? <router-link to="/">Register here</router-link></p>
     </form>
@@ -21,24 +22,37 @@
     setup() {
       const email = ref('');
       const password = ref('');
+      const errorMessage = ref();
 
       const Login = () => {
         firebase
           .auth()
           .signInWithEmailAndPassword(email.value, password.value)
           .then((data) => {
-            alert('success');
             console.log(data);
           })
           .catch((error) => {
-            alert(error.message);
-            console.log(error);
+            switch (error.code) {
+            case 'auth/invalid-email':
+              errorMessage.value = 'Invalid email';
+              break;
+            case 'auth/user-not-found':
+              errorMessage.value = 'No account with that email was found';
+              break;
+            case 'auth/wrong-password':
+              errorMessage.value = 'Incorrect password';
+              break;
+            default:
+              errorMessage.value = 'Email or password was incorrect';
+              break;
+            }
           });
       };
       return {
         Login,
         email,
-        password
+        password, 
+        errorMessage
       };
     }
   };
