3feb623ab4defc14a1ec714854df3f3a6689068b
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-07 17:25:25 +0000

    Update router.js
    fix lint

--- src/hoc/router/router.js
+++ src/hoc/router/router.js
@@ -1,15 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("../../views/Home/Home.vue"),
+    component: () => import('../../views/Home/Home.vue'),
   },
   {
     path: "/About",
     name: "About",
-    component: () => import("../../views/About/About.vue"),
+    component: () => import(''../../views/About/About.vue'),
   },
   // no route, route:
   {
