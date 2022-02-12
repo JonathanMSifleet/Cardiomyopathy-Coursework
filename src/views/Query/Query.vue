538f0d7fbeef4fe3fb240f60ef087feb0e439607
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-12 17:28:04 +0000

    Render bar charts

--- src/views/Query/Query.vue
+++ src/views/Query/Query.vue
@@ -1,6 +1,6 @@
 <template>
   <PageWrapper>
-    <h1 :class="[$style.Heading]">Statistics</h1>
+    <h1 :class="[$style.Heading]">Gene mutation data</h1>
 
     <div id="v-model-select" :class="[$style.SelectWrapper]">
       <select v-model="selectedGeneMutation" :class="[$style.Select]">
@@ -13,21 +13,29 @@
       </select>
     </div>
 
-    <MDBBtn @click="getGeneMutationData" color="success" :class="[$style.FetchDataButton]">
+    <MDBBtn @click="getGeneMutationData"
+            color="success" :class="[$style.FetchDataButton]">
       Get data
     </MDBBtn>
 
-    <div id="chart" />
+    <div v-show="displayChart" :class="[$style.ChartWrapper]">
+      <p>{{queryResults.length}} records were found with this attribute:  </p>
+      <div id="chart" :class="[$style.Chart]"/>
+    </div>
 
-    <MDBRow :class="[$style.KeyColumnRow]">
-      <p>Click a key to view a graph on it </p>
+    <MDBRow v-if="hasFetchedKeys" :class="[$style.KeyColumnRow]">
+      <p>All associated attributes with gene mutation {{ selectedGeneMutation }}.</p>
+      <p>Please click an attribute to view its data:</p>
+
       <MDBCol md="3" v-for="index in chunkedKeys.length" :key="index">
-        <li v-for="key in chunkedKeys[index-1]" :key="key"
-            @click="generateGraph(key)"
-            :class="[$style.Key]"
-        >
-          {{key}}
-        </li>
+        <ul>
+          <li v-for="key in chunkedKeys[index-1]" :key="key"
+              @click="generateGraph(key)"
+              :class="[$style.Key]"
+          >
+            {{key}}
+          </li>
+        </ul>
       </MDBCol>
     </MDBRow>
     <div>
@@ -45,7 +53,6 @@
   import chunk from 'chunk';
   import { GoogleCharts } from 'google-charts';
 
-
   export default {
     name: 'Home',
     components: {
@@ -53,6 +60,7 @@
     },
     setup() {
       const chunkedKeys = reactive([]);
+      let displayChart = ref(false);
       const geneMutations = reactive(['Please select',
                                       'ACTC',
                                       'MYBPC3',
@@ -62,13 +70,15 @@
                                       'TNNT2',
                                       'TPM1',
                                       'TTN']);
+      let hasFetchedKeys = ref(false);
       const keys = reactive([]);
+      let numObjectsQueried;
       const queryConstraints = [];
       const queryResults = reactive([]);
       let selectedGeneMutation = ref('Please select');
 
       const getGeneMutationData = async () => {
-        // cleanup();
+        cleanup();
 
         queryConstraints.push({
           fieldPath: mapGeneName(selectedGeneMutation.value),
@@ -77,11 +87,15 @@
         });
 
         const result = await generateQuery(queryConstraints);
+        numObjectsQueried = result.length;
+
         result.forEach((result) => queryResults.push(result));
         keys.push(...determineKeys(queryResults));
 
         // groups array of keys into smaller keys to be rendered in columns
         chunkedKeys.push(...chunk(keys, Math.ceil(keys.length/4)));
+
+        hasFetchedKeys.value = true;
       };
 
       const determineKeys = (data) => {
@@ -99,35 +113,71 @@
         queryResults.length = 0;
       };
 
-      const generateGraph = async (keyName) => {
-        const quantity = {true: 0, false: 0};
+      const extractDataFromResults = (keyName) => {
+        let data = {};
+        let type;
 
+        let counter = 0;
         queryResults.forEach((doc) => {
           const keyValue = doc.data[keyName];
 
           switch (typeof keyValue) {
           case 'boolean':
-            quantity[`${keyValue}`] = ++quantity[`${keyValue}`];
+            // add key to object if it doesn't exist
+            if (!data[keyValue]) data[keyValue] = [];
+
+            data[`${keyValue}`] = ++data[`${keyValue}`];
+            type = 'pie';
             break;
           case 'number':
-            // to do
+            if (!data[counter]) data[counter] = [];
+
+            data[`${counter}`] = keyValue;
+            type = 'bar';
+            counter++;
             break;
           }
-
         });
 
-        const chartData =  Object.entries(quantity);
-        chartData.unshift(['Chart thing', 'Chart amount']);
-
-        GoogleCharts.load(() => {
-          const data = GoogleCharts.api.visualization.arrayToDataTable(chartData);
-          const chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('chart'));
-          chart.draw(data);
-        });
+        return {data: Object.entries(data), type};
       };
 
-      return { chunkedKeys, generateGraph, getGeneMutationData, geneMutations,
-               keys, queryResults, selectedGeneMutation };
+      const renderGraph = (data, keyName, type) => {
+        const chartHelper = GoogleCharts.api.visualization;
+        const chartData = chartHelper.arrayToDataTable(data);
+        chartData.sort([{column: 1, asc: true}]);
+
+        const divToRenderChart = document.getElementById('chart');
+
+        const chart = type === 'pie' ? new chartHelper.PieChart(divToRenderChart) :
+          new chartHelper.ColumnChart(divToRenderChart);
+
+        chart.draw(chartData, {
+          title: `${keyName}`,
+          is3D: true,
+          vAxis: {
+            title: 'Value'
+          },
+          hAxis: {
+            title: 'Data item'
+          }
+        });
+        displayChart.value = true;
+      };
+
+      const generateGraph = async (keyName) => {
+        const {data, type} = extractDataFromResults(keyName);
+        data.unshift(['Test', 'Value']);
+
+        data.forEach((curData) => curData[0] = curData[0][0].toUpperCase()
+          + curData[0].slice(1).toLowerCase()
+        );
+
+        GoogleCharts.load(() => renderGraph(data, keyName, type));
+      };
+
+      return { chunkedKeys, displayChart, generateGraph, getGeneMutationData, geneMutations,
+               hasFetchedKeys, keys, numObjectsQueried, queryResults, selectedGeneMutation };
     }
   };
 </script>
