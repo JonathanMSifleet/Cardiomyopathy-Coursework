b83efbd4a8b40df880f9eec423c9d490afc7b447
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-12 19:41:25 +0000

    Add recommended vue eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,8 @@ module.exports = {
       'never'
     ],
     'eol-last': [2, 'windows'],
-    indent: ['error', 2, { "SwitchCase": 1 }],
+    'func-style': ['error', 'expression'],
+    indent: ['error', 2, { 'SwitchCase': 1 }],
     'linebreak-style': [
       'error',
       'windows'
@@ -28,13 +29,28 @@ module.exports = {
       }
     ],
     'no-trailing-spaces': 'error',
-    "quotes": [
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error', {
+        'disallowPrototype': true,
+        'singleReturnOnly': false,
+        'classPropertiesAllowed': false
+      }],
+    'quotes': [
       'error',
       'single'
     ],
     semi: ['error', 'always'],
     'vue/html-indent': ['error', 2, {
       baseIndent: 1
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': {
+        'max': 3
+      },
+      'multiline': {
+        'max': 1
+      }
     }],
     'vue/script-indent': [
       'error',
@@ -43,8 +59,15 @@ module.exports = {
     ]
   },
   extends: [
+    'plugin:vue/base',
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:import/recommended'
+  ],
+  "plugins": [
+    "prefer-arrow"
   ],
   overrides: [
     {
