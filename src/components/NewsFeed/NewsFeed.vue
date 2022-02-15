a13c5e474791032091b6ffdb8aaa6b5b6f12b5c7
Humzah99 <humzah99@live.co.uk>
2022-02-15 01:11:54 +0000

    Attempting to create a modal for each news feed card

--- src/components/NewsFeed/NewsFeed.vue
+++ src/components/NewsFeed/NewsFeed.vue
@@ -1,19 +1,36 @@
 <template>
-  <div id="newsFeed" class="testcard">
+  <div id="newsFeed" v-for="item of items" :key="item.title">
     <h1>News Feed</h1>
-
-    <div class="cards" v-for="item of items" :key="item.title">
+    <div id="myModal" class="modal" v-bind:style="{ display: isModalActive ? 'block' : 'none' }">
+      <div class="modal-content">
+        <div class="modal-header">
+          <span class="close" @click="toggleModal">&times;</span>
+          <h2>{{ item.title }}</h2>
+        </div>
+        <div class="modal-body">
+          <p>{{ item.description }}</p>
+        </div>
+        <div class="modal-footer">
+          <h3>{{ item.pubDate }}</h3>
+        </div>
+      </div>
+    </div>
+    <div class="cards">
       <div class="card">
         <img class="card-img-top" v-bind:src="item.enclosure.link" />
         <div class="card-body">
-          <h2 class="card-title " v-bind:class="[readMore ? 'remove-style' : '']" >{{ item.title }}</h2>
+          <h2 class="card-title" v-bind:class="[readMore ? 'remove-style' : '']">
+            {{ item.title }}
+            <button id="myBtn" @click="toggleModal">View content</button>
+          </h2>
           <!-- <p>
             {{ item.description }}
             <a href="#1">Read more...</a>
           </p>
           <p>{{ item.pubDate }}</p> -->
-          <br>
-          <button @click="toggleClass" v-html="readMore ? 'Read less...' : 'Read more...'"></button>
+          <br />
+          <button @click="toggleClass" v-html="readMore ? 'Read less...' : 'Read more...'"></button
+          >
         </div>
       </div>
     </div>
@@ -21,12 +38,12 @@
 </template>
 
 <script>
-
 import { ref } from "vue";
 export default {
   setup() {
     let items = ref([]);
     let readMore = ref(false);
+    let isModalActive = ref(false);
 
     async function getRss() {
       // eslint-disable-next-line max-len
@@ -40,18 +57,49 @@ export default {
 
     function toggleClass() {
       readMore.value = !readMore.value;
- //   readmore.value = true;
     }
+
+    function toggleModal() {
+      isModalActive.value = !isModalActive.value;
+    }
+
+    // // Get the modal
+    // var modal = document.getElementById("myModal");
+
+    // // Get the button that opens the modal
+    // var btn = document.getElementById("myBtn");
+
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+
+    // // When the user clicks on the button, open the modal
+    // btn.onclick = function () {
+    //   modal.style.display = "block";
+    // };
+
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function () {
+    //   modal.style.display = "none";
+    // };
+
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function (event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // };
     getRss();
     return {
       items,
       readMore,
-      toggleClass
+      toggleClass,
+      toggleModal,
+      isModalActive,
     };
   },
-}
+};
 </script>
 
 <style>
-  @import "./NewsFeed.scss";
+@import "./NewsFeed.scss";
 </style>
