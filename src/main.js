e9c4841094dcfb3ec86de1d836c01764d2f5f9b7
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-02-15 16:37:31 +0000

    Revert "Merge branch 'Login/Registration'"
    This reverts commit 0fe94e9ff0425d5590d0e4290a90b7b62d26e91c, reversing
    changes made to 5276bdc4bcf16eef56d8dffe377324a0dea2ddb1.

--- src/main.js
+++ src/main.js
@@ -4,21 +4,9 @@ import './assets/styles/global.scss';
 import App from './App.vue';
 import router from './hoc/router/router.js';
 import store from './hoc/store/store.js';
-import { auth } from './firebase/config'
-import { onAuthStateChanged } from 'firebase/auth'
 import { createApp } from 'vue';
-// import axios from 'axios';
 
-let app = null;
-
-onAuthStateChanged(auth, ()=>{
-  if (app == null){
-    app = createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app');
-  }
-});
-//establishes user state (logged in/out) before rendering components
-
-// app.use(axios);
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
