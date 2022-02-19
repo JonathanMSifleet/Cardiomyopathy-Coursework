58c43999e44aec3de1b5d3234f1b25bc6b34aeae
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-19 01:01:16 +0000

    Enable selecting gene mutation

--- src/views/Query/Query.vue
+++ src/views/Query/Query.vue
@@ -4,122 +4,145 @@
       Gene mutation data
     </h1>
 
-    <Spinner v-if="isFetchingData" />
-    <div v-else :class="[$style.ComponentWrapper]">
-      <div v-if="filters.length !== 0" :class="[$style.FiltersWrapper]">
-        <p> Filters: </p>
-        <ul>
-          <li
-            v-for="(filter, index) in filters"
-            :key="index"
-            :class="[$style.FilterListItem]"
-            @click="deleteFilter(index)"
-          >
-            {{ filter.fieldPath }} {{ filter.opStr }} {{ filter.value }}
-            <span :class="[$style.DeleteFilterSpan]">x</span>
-          </li>
-        </ul>
-      </div>
+    <div :class="[$style.AdvancedModeSwitchWrapper]">
+      <MDBSwitch
+        v-model="useAdvancedMode"
+        :class="[$style.AdvancedModeSwitch]"
+        label="Advanced search"
+      />
+    </div>
 
-      <p>
-        Please add a filter
-        <span :class="[$style.FilterInstruction]">(using text in brackets if it is a default column)</span>:
-      </p>
-
-      <div :class="[$style.FilterWrapper]">
-        <div :class="[$style.InputWrapper]">
-          <MDBInput
-            v-model="queryInput"
-            label="Attribute"
-            type="text"
-            :class="[$style.Input]"
-          />
-        </div>
-
-        <div :class="[$style.SelectWrapper]">
-          <select v-model="selectedOperator" :class="[$style.Select, 'form-select']">
-            <option
-              v-for="operation in Object.entries(fireStoreOperators)"
-              :key="operation"
-              :disabled="operation === 'Please select'"
+    <div v-if="useAdvancedMode">
+      <Spinner v-if="isFetchingData" />
+      <div v-else :class="[$style.ComponentWrapper]">
+        <div v-if="filters.length !== 0" :class="[$style.FiltersWrapper]">
+          <p> Filters: </p>
+          <ul>
+            <li
+              v-for="(filter, index) in filters"
+              :key="index"
+              :class="[$style.FilterListItem]"
+              @click="deleteFilter(index)"
             >
-              {{ operation[1] }}
-            </option>
-          </select>
+              {{ filter.fieldPath }} {{ filter.opStr }} {{ filter.value }}
+              <span :class="[$style.DeleteFilterSpan]">x</span>
+            </li>
+          </ul>
         </div>
 
-        <div :class="[$style.InputWrapper]">
-          <MDBInput
-            v-model="queryOperand"
-            label="Value"
-            type="text"
-            :class="[$style.Input]"
-          />
-        </div>
+        <p>
+          Please add a filter
+          <span :class="[$style.FilterInstruction]">(using text in brackets if it is a default column)</span>:
+        </p>
 
-        <MDBBtn
-          color="primary"
-          :disabled="!canSubmitFilter"
-          @click="addFilter"
-        >
-          Add filter
-        </MDBBtn>
-      </div>
+        <div :class="[$style.FilterWrapper]">
+          <div :class="[$style.InputWrapper]">
+            <MDBInput
+              v-model="queryInput"
+              label="Attribute"
+              type="text"
+              :class="[$style.Input]"
+            />
+          </div>
 
-      <div :class="[$style.CheckboxWrapper]">
-        <p>Selected columns:</p>
-        <MDBCheckbox
-          v-for="(key, index) in mapKeyNameToWords(Object.keys(optionalTableKeys)).sort(Intl.Collator().compare)"
-          :key="index"
-          v-model="activeTableKeys[key]"
-          :label="mapKeyNameToWords(key)"
-          inline
-          @change="toggleKey(key)"
-        />
-      </div>
-
-      <p :class="[$style.GraphPrompt]">
-        Click a table header to view a graph on the data
-      </p>
-      <div :class="[$style.TableWrapper]">
-        <p>Results ({{ renderableResults.length }}):</p>
-
-        <Spinner v-if="isLoadingGraph" />
-        <div id="chart" />
-
-        <MDBTable
-          bordered
-          striped
-          :class="[$style.Table]"
-        >
-          <thead>
-            <tr>
-              <th
-                v-for="(key, index) in Object.entries(activeTableKeys)"
-                :key="index"
-                scope="col"
-                :class="[$style.TableHeaderCell]"
-                @click="selectGraphKey(key[0])"
+          <div :class="[$style.SelectWrapper]">
+            <select v-model="selectedOperator" :class="[$style.Select, 'form-select']">
+              <option
+                v-for="operation in Object.entries(fireStoreOperators)"
+                :key="operation"
+                :disabled="operation === 'Please select'"
               >
-                <p :class="[$style.TableHeaderText]">
-                  {{ mapKeyNameToWords(key[0]) }}
-                  {{ key[0] !== mapKeyNameToWords(key[0])
-                    ? '(' + key[0] + ')'
-                    : ''
-                  }}
-                </p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(dataItem, outerIndex) in renderableResults" :key="outerIndex">
-              <td v-for="(key, innerIndex) in Object.entries(activeTableKeys)" :key="innerIndex">
-                {{ dataItem[key[0]] }}
-              </td>
-            </tr>
-          </tbody>
-        </MDBTable>
+                {{ operation[1] }}
+              </option>
+            </select>
+          </div>
+
+          <div :class="[$style.InputWrapper]">
+            <MDBInput
+              v-model="queryOperand"
+              label="Value"
+              type="text"
+              :class="[$style.Input]"
+            />
+          </div>
+
+          <MDBBtn
+            color="primary"
+            :disabled="!canSubmitFilter"
+            @click="addFilter"
+          >
+            Add filter
+          </MDBBtn>
+        </div>
       </div>
+    </div>
+
+    <div v-else>
+      <p :class="[$style.GeneMutationSelection]">
+        Select a gene mutation:
+      </p>
+      <div :class="[$style.SelectWrapper, $style.GeneMutationWrapper]">
+        <select v-model="selectedGeneMutation" :class="[$style.Select, 'form-select']">
+          <option v-for="geneMutation in geneMutations" :key="geneMutation">
+            {{ geneMutation }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <div :class="[$style.CheckboxWrapper]">
+      <p>Selected columns:</p>
+      <MDBCheckbox
+        v-for="(key, index) in mapKeyNameToWords(Object.keys(optionalTableKeys)).sort(Intl.Collator().compare)"
+        :key="index"
+        v-model="activeTableKeys[key]"
+        :label="mapKeyNameToWords(key)"
+        inline
+        @change="toggleKey(key)"
+      />
+    </div>
+
+    <p :class="[$style.GraphPrompt]">
+      Click a table header to view a graph on the data
+    </p>
+    <div :class="[$style.TableWrapper]">
+      <p>Results ({{ renderableResults.length }}):</p>
+
+      <Spinner v-if="isLoadingGraph" />
+      <div id="chart" />
+
+      <MDBTable
+        bordered
+        striped
+        :class="[$style.Table]"
+      >
+        <thead>
+          <tr>
+            <th
+              v-for="(key, index) in Object.entries(activeTableKeys)"
+              :key="index"
+              scope="col"
+              :class="[$style.TableHeaderCell]"
+              @click="selectGraphKey(key[0])"
+            >
+              <p :class="[$style.TableHeaderText]">
+                {{ mapKeyNameToWords(key[0]) }}
+                {{ key[0] !== mapKeyNameToWords(key[0])
+                  ? '(' + key[0] + ')'
+                  : null
+                }}
+              </p>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(dataItem, outerIndex) in renderableResults" :key="outerIndex">
+            <td v-for="(key, innerIndex) in Object.entries(activeTableKeys)" :key="innerIndex">
+              {{ dataItem[key[0]] }}
+            </td>
+          </tr>
+        </tbody>
+      </MDBTable>
     </div>
   </PageWrapper>
 </template>
@@ -130,14 +153,14 @@
   import determineKeys from '../../utils/determineKeys';
   import fetchDocuments from '../../utils/fetchDocuments';
   import { GoogleCharts } from 'google-charts';
-  import { MDBBtn, MDBCheckbox, MDBInput, MDBTable } from 'mdb-vue-ui-kit';
+  import { MDBBtn, MDBCheckbox, MDBInput, MDBSwitch, MDBTable } from 'mdb-vue-ui-kit';
   import { isValid } from '../../utils/validationFunctions';
   import { reactive, ref, watch } from 'vue';
 
   export default {
     name: 'Query',
     components: {
-      MDBBtn, MDBCheckbox, MDBInput, MDBTable, PageWrapper, Spinner
+      MDBBtn, MDBCheckbox, MDBInput, MDBSwitch, MDBTable, PageWrapper, Spinner
     },
     setup() {
       const activeTableKeys = ref({
@@ -173,13 +196,26 @@
       let isFetchingData = ref(false);
       let isLoadingGraph = ref(false);
       let canSubmitFilter = ref(false);
+      const geneMutations = ref([
+        'MYH7',
+        'MYBPC3',
+        'TNNT2',
+        'ACTC',
+        'TPM1',
+        'TNNCI',
+        'TNNI3',
+        'MYL2',
+        'TTN'
+      ]);
       let optionalTableKeys = ref([]);
       let queryInput = ref('');
       let queryOperand = ref('');
       let renderableResults = ref([]);
+      let selectedGeneMutation = ref('');
       let selectedGraphKey = ref();
       let selectedOperator = ref('Please select');
       let textOnlyTableKeys = [];
+      let useAdvancedMode = ref(false);
 
       (async () => {
         isFetchingData.value = true;
@@ -363,9 +399,16 @@
         (queryInput.value !== '' && selectedOperator.value !== 'Please select' && queryOperand.value !== '')
       );
 
+      watch(selectedGeneMutation, () => {
+        filters = [];
+        renderableResults.value = allDocuments
+          .filter(doc => doc[`${selectedGeneMutation.value}`]);
+      });
+
       return { activeTableKeys, addFilter, canSubmitFilter, deleteFilter, displayChart, filters, fireStoreOperators,
-               generateGraph, isFetchingData, isLoadingGraph, mapKeyNameToWords, optionalTableKeys, queryInput,
-               queryOperand, renderableResults, selectedOperator, selectGraphKey, toggleKey };
+               geneMutations, generateGraph, isFetchingData, isLoadingGraph, mapKeyNameToWords, optionalTableKeys,
+               queryInput, queryOperand, renderableResults, selectedGeneMutation, selectGraphKey, selectedOperator,
+               toggleKey, useAdvancedMode };
     }
   };
 </script>
