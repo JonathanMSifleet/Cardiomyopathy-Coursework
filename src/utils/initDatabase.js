b88dfe214a6672b096579818484a662c69533f04
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-20 18:40:40 +0000

    Minor tweaks

--- src/utils/initDatabase.js
+++ src/utils/initDatabase.js
@@ -2,11 +2,11 @@ import { enableIndexedDbPersistence, getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import firebaseDetails from '../constants/firebaseDetails';
 
-const getDatabase = async () => {
+const initDatabase = async () => {
   const db = getFirestore(initializeApp(firebaseDetails.details));
   if (process.env.DEVELOPMENT) await enableIndexedDbPersistence(db);
 
   return db;
 };
 
-export default getDatabase;
+export default initDatabase;
