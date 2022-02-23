fe7643d67d3760d367ac619dce45a92b0f123963
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-02-23 22:58:02 +0000

    Handle remaining upload formats

--- src/components/FileUpload/FileUpload.vue
+++ src/components/FileUpload/FileUpload.vue
@@ -1,72 +1,137 @@
 <template>
   <div :class="$style.FileUploadWrapper">
-    <MDBFile v-model="fileUpload" :class="$style.FileUpload" />
+    <div :class="$style.RadioGroup">
+      <MDBRadio
+        v-for="(formatType) in formatTypes"
+        :key="formatType"
+        v-model="radioFormat"
+        :label="formatType"
+        :value="formatType"
+        inline
+      />
+    </div>
+
+    <div :class="$style.FileUploadComponentWrapper">
+      <MDBFile v-model="fileUpload" :class="$style.FileUpload" />
+    </div>
+
+    <ButtonSpinner v-if="isSubmitting" />
+    <MDBBtn
+      v-else
+      color="primary"
+      :class="$style.SubmitButton"
+      :disabled="fileUpload.length === 0"
+      @click="shouldSubmitData = true"
+    >
+      Submit
+    </MDBBtn>
   </div>
 
-  <ButtonSpinner v-if="isSubmitting" />
-  <MDBBtn
-    v-else
-    color="primary"
-    :class="$style.SubmitButton"
-    :disabled="fileUpload.length === 0"
-    @click="shouldSubmitData = true"
-  >
-    Submit
-  </MDBBtn>
+  <div :class="$style.InstructionsWrapper">
+    <MDBBtn
+      color="primary"
+      aria-controls="collapsibleContent1"
+      :aria-expanded="showFormatInstructions"
+      :class="$style.CollapseButton"
+      @click="showFormatInstructions = !showFormatInstructions"
+    >
+      How to format {{ radioFormat }}
+    </MDBBtn>
+    <!-- do not format this pre tag! -->
+    <pre
+      v-if="showFormatInstructions"
+      div
+      :class="$style.InstructionText"
+    >{{ determineFormatInstructions() }}</pre>
+  </div>
+
+  <p v-if="statusMessage !== ''" :class="$style.StatusMessage">
+    {{ statusMessage }}
+  </p>
 </template>
 
 <script>
-  import store from '../../services/store';
-  import { MDBBtn, MDBFile } from 'mdb-vue-ui-kit';
-  import { ref, watch } from 'vue';
-  import { collection, addDoc } from 'firebase/firestore';
   import ButtonSpinner from '../ButtonSpinner/ButtonSpinner.vue';
+  import YAML from 'yaml';
+  import csv from 'csvtojson';
+  import store from '../../services/store';
+  import { MDBBtn, MDBFile, MDBRadio } from 'mdb-vue-ui-kit';
+  import { XMLParser } from 'fast-xml-parser';
+  import { collection, addDoc } from 'firebase/firestore';
+  import { ref, watch } from 'vue';
 
   export default {
     components: {
-      ButtonSpinner, MDBBtn, MDBFile
+      ButtonSpinner, MDBBtn, MDBFile, MDBRadio
     },
     setup() {
       let fileUpload = ref([]);
-      let isSubmitting = ref(false);
+      const formatTypes = ref([
+        'JSON',
+        'YAML',
+        'XML',
+        'CSV'
+      ]);
       const shouldSubmitData = ref(false);
+      let isSubmitting = ref(false);
+      let radioFormat = ref('JSON');
+      let showFormatInstructions = ref(false);
+      let statusMessage = ref('');
 
-      const handleFileUpload = async (parsableData) => {
+      const determineFormatInstructions = () => {
+        const intermediateText = require(`./exampleData/${radioFormat.value.toLowerCase()}.txt`);
+        return intermediateText.default.toString().replaceAll('\\n', '\n').trim();
+      };
+
+      const handleFileFormat = async (parsableData) => {
         isSubmitting.value = true;
 
         try {
-          const parsedData = JSON.parse(parsableData);
-
-          const database = await store.database;
-          const tasks = [];
-
-          parsedData.forEach(doc => {
-            tasks.push(addDoc(collection(database, 'experimentalData'), doc));
-          });
-
-          await Promise.all(tasks);
+          switch (radioFormat.value) {
+          case 'YAML':
+            return uploadData(YAML.parse(parsableData));
+          case 'XML':
+            return uploadData(new XMLParser().parse(parsableData).root.row);
+          case 'CSV':
+            return uploadData(await csv().fromString(parsableData));
+          default:
+            return uploadData(JSON.parse(parsableData));
+          }
         } catch (error) {
-          console.error(error);
+          isSubmitting.value = false;
+          statusMessage.value = 'Data formatted incorrectly';
         }
+      };
+
+      const uploadData = async (data) => {
+        const database = await store.database;
+        const tasks = [];
+
+        data.forEach(doc => tasks.push(addDoc(collection(database, 'experimentalData'), doc)));
+
+        await Promise.all(tasks);
 
         shouldSubmitData.value = false;
         fileUpload.value = [];
 
         isSubmitting.value = false;
+        statusMessage.value = 'Data uploaded successfully';
       };
 
       watch([fileUpload, shouldSubmitData], () => {
+        statusMessage.value = '';
         if (!shouldSubmitData.value) return;
 
         const reader = new FileReader();
 
         reader.readAsText(fileUpload.value[0]);
         reader.onload = () => {
-          handleFileUpload(reader.result);
+          handleFileFormat(reader.result);
         };
       });
 
-      return { fileUpload, isSubmitting, shouldSubmitData };
+      return { determineFormatInstructions, fileUpload, formatTypes, isSubmitting, radioFormat,
+               shouldSubmitData,showFormatInstructions, statusMessage };
     }
   };
 </script>
