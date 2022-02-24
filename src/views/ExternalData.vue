64f51e66e778961b9c02cc8f4d77283567d44618
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-02-24 17:10:18 +0000

    Revert 367d933

---
+++ src/views/ExternalData.vue
@@ -0,0 +1,13 @@
+<template>
+  <externalData />
+</template>
+
+<script>
+  import externalData from '../components/externalData.vue';
+
+  export default {
+    components: { externalData }
+  };
+</script>
+
+<style></style>
