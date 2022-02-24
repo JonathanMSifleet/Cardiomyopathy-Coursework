b93b5ab091e941fa78cbb615189bf24019241b39
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-02-24 16:11:45 +0000

    Remove default placement

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <PageWrapper>
-    <FileUpload />
     <NewsFeed />
   </PageWrapper>
 </template>
 
 <script>
-  import FileUpload from '../../components/FileUpload/FileUpload.vue';
   import NewsFeed from '../../components/NewsFeed/NewsFeed.vue';
   import PageWrapper from '../../components/PageWrapper/PageWrapper.vue';
 
@@ -16,19 +14,6 @@
       FileUpload, NewsFeed, PageWrapper
     },
     setup() {
-      // const { currentUser } = getUser();
-      // const name = ref('');
-
-      // onBeforeMount(() => {
-      //   if (currentUser.value) {
-      //     name.value = currentUser.value.displayName;
-      //   }
-      // });
-
-      // return {
-      //   name,
-      //   currentUser
-      // };
     }
   };
 </script>
