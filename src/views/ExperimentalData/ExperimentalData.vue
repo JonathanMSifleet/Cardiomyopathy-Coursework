63765e74b6b83c2d18a705bfe2385beb1978f81b
Humzah99 <humzah99@live.co.uk>
2022-03-02 18:12:36 +0000

    Styling the experimental data form

--- src/views/ExperimentalData/ExperimentalData.vue
+++ src/views/ExperimentalData/ExperimentalData.vue
@@ -6,8 +6,8 @@
         <MDBCardTitle>Gene Mutation Data</MDBCardTitle>
         <MDBCardText>
           <form>
-            <MDBRow>
-              <MDBCol md="4">
+            <MDBRow class="mt-5 mb-5">
+              <MDBCol md="6">
                 <select
                   id="geneMutations"
                   v-model="selectedMutation"
@@ -26,41 +26,48 @@
                   </option>
                 </select>
               </MDBCol>
-            </MDBRow>
-            <MDBRow class="mb-3">
-              <MDBCol>
-                <div v-if="showGenderInput">
-                  <span :class="$style.DeleteInput" @click="showGenderInput = !showGenderInput"> x </span>
-                  <MDBInput
-                    v-model="info[Gender]"
-                    type="text"
-                    label="Gender"
-                  />
-                </div>
 
-                <div v-for="input in dataInputs" :key="input" :class="$style.InputWrapper">
-                  <span :class="$style.DeleteInput" @click="deleteInput(input)"> x </span>
-
-                  <MDBInput
-                    v-model="info[input]"
-                    type="number"
-                    :label="mapKeyToWords(input)"
-                    :class="$style.ExperimentalDataInput"
-                  />
-                </div>
-
-                <MDBInput
-                  v-model="newInput"
-                  type="text"
-                  label="New input name"
-                />
-                <MDBBtn
-                  color="primary"
-                  @click="createNewInput"
+              <MDBCol md="6" v-if="showGenderInput">
+                <MDBInput v-model="info[Gender]" type="text" label="Gender" />
+                <span
+                  :class="$style.DeleteInput"
+                  @click="showGenderInput = !showGenderInput"
                 >
-                  Add new input
-                </MDBBtn>
+                  x
+                </span>
               </MDBCol>
+            </MDBRow>
+
+            <MDBRow class="mb-5">
+              <MDBCol
+                md="4"
+                v-for="input in dataInputs"
+                :key="input"
+                class="mb-4"
+                :class="$style.InputWrapper"
+              >
+                <MDBInput
+                  v-model="info[input]"
+                  type="number"
+                  :label="mapKeyToWords(input)"
+                  :class="$style.ExperimentalDataInput"
+                />
+                <span :class="$style.DeleteInput" @click="deleteInput(input)">
+                  x
+                </span>
+              </MDBCol>
+                <MDBCol md="4">
+                  <MDBInput
+                    v-model="newInput"
+                    type="text"
+                    label="New input name"
+                  />
+                </MDBCol>
+                <MDBCol md="2">
+                  <MDBBtn color="primary" @click="createNewInput">
+                    Add new input
+                  </MDBBtn>
+                </MDBCol>
             </MDBRow>
             <MDBRow class="mb-4">
               <MDBCol md="4">
@@ -114,141 +121,152 @@
 </template>
 
 <script>
-  import PageWrapper from '../../components/PageWrapper/PageWrapper.vue';
-  import getUser from '../../composables/getUser';
-  import mapKeyToWords from '../../utils/mapKeyToWords';
-  import store from '../../services/store';
-  import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
-  import { ref, reactive } from '@vue/reactivity';
-  import { useRouter } from 'vue-router';
-  import { watchEffect } from 'vue';
-  import {
-    MDBRow,
-    MDBCol,
-    MDBInput,
-    MDBCheckbox,
+import PageWrapper from "../../components/PageWrapper/PageWrapper.vue";
+import getUser from "../../composables/getUser";
+import mapKeyToWords from "../../utils/mapKeyToWords";
+import store from "../../services/store";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { ref, reactive } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+import { watchEffect } from "vue";
+import {
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCheckbox,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter,
+} from "mdb-vue-ui-kit";
+
+export default {
+  name: "ExperimentalData",
+  components: {
     MDBBtn,
     MDBCard,
     MDBCardBody,
+    MDBCardFooter,
     MDBCardHeader,
-    MDBCardTitle,
     MDBCardText,
-    MDBCardFooter
-  } from 'mdb-vue-ui-kit';
+    MDBCardTitle,
+    MDBCheckbox,
+    MDBCol,
+    MDBInput,
+    MDBRow,
+    PageWrapper,
+  },
+  setup() {
+    const { currentUser } = getUser();
+    let newInput = ref("");
+    const router = useRouter();
+    const geneMutations = reactive([
+      "Please select",
+      "MYH7",
+      "MYBPC3",
+      "TNNT2",
+      "ACTC",
+      "TPM1",
+      "TNNCI",
+      "TNNI3",
+      "MYL2",
+      "TTN",
+    ]);
+    const selectedMutation = ref("Please select");
+    let showGenderInput = ref(true);
+    const info = reactive({
+      userId: currentUser.value.uid,
+      ledv: "",
+      redv: "",
+      lesv: "",
+      resv: "",
+      lvef: "",
+      rvef: "",
+      lvmass: "",
+      rvmass: "",
+      lsv: "",
+      rsv: "",
+      gender: "",
+      fibrosis: false,
+      ageAtMri: false,
+      apicalHcm: false,
+      suddenCardiacDeath: false,
+      hypertension: false,
+      diabetes: false,
+      myectomy: false,
+    });
+    const dataInputs = reactive([
+      "ledv",
+      "redv",
+      "lesv",
+      "resv",
+      "lvef",
+      "rvef",
+      "lvmass",
+      "lsv",
+      "rsv",
+      "AgeatMRI",
+    ]);
 
-  export default {
-    name: 'ExperimentalData',
-    components: {
-      MDBBtn,
-      MDBCard,
-      MDBCardBody,
-      MDBCardFooter,
-      MDBCardHeader,
-      MDBCardText,
-      MDBCardTitle,
-      MDBCheckbox,
-      MDBCol,
-      MDBInput,
-      MDBRow,
-      PageWrapper
-    },
-    setup() {
-      const { currentUser } = getUser();
-      let newInput = ref('');
-      const router = useRouter();
-      const geneMutations = reactive([
-        'Please select',
-        'MYH7',
-        'MYBPC3',
-        'TNNT2',
-        'ACTC',
-        'TPM1',
-        'TNNCI',
-        'TNNI3',
-        'MYL2',
-        'TTN'
-      ]);
-      const selectedMutation = ref('Please select');
-      let showGenderInput = ref(true);
-      const info = reactive({
-        userId: currentUser.value.uid,
-        ledv: '',
-        redv: '',
-        lesv: '',
-        resv: '',
-        lvef: '',
-        rvef: '',
-        lvmass: '',
-        rvmass: '',
-        lsv: '',
-        rsv: '',
-        gender: '',
-        fibrosis: false,
-        ageAtMri: false,
-        apicalHcm: false,
-        suddenCardiacDeath: false,
-        hypertension: false,
-        diabetes: false,
-        myectomy: false
+    const createNewInput = () => {
+      if (newInput.value) dataInputs.push(newInput.value);
+      newInput.value = "";
+    };
+
+    const deleteInput = (key) => {
+      if (!dataInputs.includes(key)) return;
+
+      dataInputs.splice(dataInputs.indexOf(key), 1);
+      delete info[key];
+    };
+
+    const experimentalData = async () => {
+      for (const key in info) {
+        if (info[key] === "" || info[key] === undefined) delete info[key];
+      }
+
+      if (document.getElementById("geneMutations").value === "Please select") {
+        alert("Please select a valid Gene mutation");
+        return;
+      }
+
+      const docRef = await addDoc(collection(await store.database, "hcmData"), {
+        ...info,
+        GeneMutation: selectedMutation.value,
+        createdAt: serverTimestamp(),
+        deletedAt: null,
       });
-      const dataInputs = reactive([
-        'ledv',
-        'redv',
-        'lesv',
-        'resv',
-        'lvef',
-        'rvef',
-        'lvmass',
-        'lsv',
-        'rsv',
-        'AgeatMRI'
-      ]);
+      console.log("Document written with ID: ", docRef.id);
 
-      const createNewInput = () => {
-        if (newInput.value) dataInputs.push(newInput.value);
-        newInput.value = '';
-      };
+      alert("A new document has been added.");
+      router.push("/");
+    };
 
-      const deleteInput = (key) => {
-        if (!dataInputs.includes(key)) return;
+    //redirects anytime the user is signed out
+    //whilst still on this page
+    watchEffect(() => {
+      if (!currentUser.value) router.push("/login");
+    });
 
-        dataInputs.splice(dataInputs.indexOf(key), 1);
-        delete info[key];
-      };
-
-      const experimentalData = async () => {
-        for (const key in info) {
-          if (info[key] === '' || info[key] === undefined) delete info[key];
-        }
-
-        if (document.getElementById('geneMutations').value === 'Please select') {
-          alert('Please select a valid Gene mutation'); return;
-        }
-
-        const docRef = await addDoc(collection(await store.database, 'hcmData'), {
-          ...info,
-          GeneMutation: selectedMutation.value,
-          createdAt: serverTimestamp(),
-          deletedAt: null
-        });
-        console.log('Document written with ID: ', docRef.id);
-
-        alert('A new document has been added.');
-        router.push('/');
-      };
-
-      //redirects anytime the user is signed out
-      //whilst still on this page
-      watchEffect(() => {
-        if (!currentUser.value) router.push('/login');
-      });
-
-      return { createNewInput, dataInputs, deleteInput, experimentalData, geneMutations, info, mapKeyToWords, newInput,
-               selectedMutation, showGenderInput };
-    }
-  };
+    return {
+      createNewInput,
+      dataInputs,
+      deleteInput,
+      experimentalData,
+      geneMutations,
+      info,
+      mapKeyToWords,
+      newInput,
+      selectedMutation,
+      showGenderInput,
+    };
+  },
+};
 </script>
 
 <style module lang="scss">
-  @import "./ExperimentalData.module.scss";
+@import "./ExperimentalData.module.scss";
 </style>
