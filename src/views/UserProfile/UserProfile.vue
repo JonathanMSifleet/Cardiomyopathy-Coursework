67d518a2b4d311968d9fffd70138bb8145adaabf
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-03-04 18:27:52 +0000

    Code styling

--- src/views/UserProfile/UserProfile.vue
+++ src/views/UserProfile/UserProfile.vue
@@ -150,7 +150,9 @@
                 required
               />
 
-              <MDBBtn type="submit" color="primary"> Update </MDBBtn>
+              <MDBBtn type="submit" color="primary">
+                Update
+              </MDBBtn>
             </form>
           </MDBCardText>
         </MDBCardBody>
@@ -160,8 +162,8 @@
       <MDBCol md="12" class="text-center">
         <MDBBtn
           color="primary"
-          @click="showCards"
           :disabled="experimentalData.length === 0"
+          @click="showCards"
         >
           <p
             :class="[
@@ -219,46 +221,26 @@
 </template>
 
 <script>
-import PageWrapper from "../../components/PageWrapper/PageWrapper.vue";
-import getUser from "../../composables/getUser";
-import mapKeyToWords from "../../utils/mapKeyToWords";
-import store from "../../services/store";
-import { auth } from "../../firebase/config";
-import { onMounted, reactive, ref, watchEffect } from "vue";
-import { updateProfile } from "firebase/auth";
-import { useRouter } from "vue-router";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  serverTimestamp,
-  updateDoc,
-  where,
-} from "firebase/firestore";
-import {
-  MDBBtn,
-  MDBCard,
-  MDBCardBody,
-  MDBCardHeader,
-  MDBCardText,
-  MDBCardFooter,
-  MDBCol,
-  MDBIcon,
-  MDBInput,
-  MDBModal,
-  MDBModalBody,
-  MDBModalFooter,
-  MDBModalHeader,
-  MDBModalTitle,
-  MDBRow,
-  MDBTable,
-} from "mdb-vue-ui-kit";
-
-export default {
-  components: {
+  import PageWrapper from '../../components/PageWrapper/PageWrapper.vue';
+  import getUser from '../../composables/getUser';
+  import mapKeyToWords from '../../utils/mapKeyToWords';
+  import store from '../../services/store';
+  import { auth } from '../../firebase/config';
+  import { onMounted, reactive, ref, watchEffect } from 'vue';
+  import { updateProfile } from 'firebase/auth';
+  import { useRouter } from 'vue-router';
+  import {
+    collection,
+    deleteDoc,
+    doc,
+    getDoc,
+    getDocs,
+    query,
+    serverTimestamp,
+    updateDoc,
+    where
+  } from 'firebase/firestore';
+  import {
     MDBBtn,
     MDBCard,
     MDBCardBody,
@@ -274,188 +256,188 @@ export default {
     MDBModalHeader,
     MDBModalTitle,
     MDBRow,
-    MDBTable,
-    PageWrapper,
-  },
-  setup() {
-    const { currentUser } = getUser();
-    const firstName = ref("");
-    const lastName = ref("");
-    const address = ref("");
-    const email = ref("");
-    const phoneNumber = ref("");
-    const experimentalData = ref([]);
-    const deleteConfirmationModal = ref(false);
-    const currentDocId = ref("");
-    const currentEntity = ref([]);
-    const router = useRouter();
-    const cardsVisible = ref(false);
-    const showCardContainer = ref(false);
-    const experimentalDataModal = ref(false);
-    const tableHeaders = reactive([
-      "Gene Mutation",
-      "ledv",
-      "redv",
-      "lesv",
-      "resv",
-      "lvef",
-      "rvef",
-      "lvmass",
-      "lsv",
-      "rsv",
-      "Gender",
-      "Fibrosis",
-      "Age at MRI",
-      "Apical HCM",
-      "Sudden Cardiac Death",
-      "Hypertension",
-      "Diabetes",
-      "Myectomy",
-      "Date Created",
-      "Actions",
-    ]);
+    MDBTable
+  } from 'mdb-vue-ui-kit';
 
-    onMounted(() => {
-      userIdentity();
-      getExperimentalDataByUserId();
-    });
+  export default {
+    components: {
+      MDBBtn,
+      MDBCard,
+      MDBCardBody,
+      MDBCardHeader,
+      MDBCardText,
+      MDBCardFooter,
+      MDBCol,
+      MDBIcon,
+      MDBInput,
+      MDBModal,
+      MDBModalBody,
+      MDBModalFooter,
+      MDBModalHeader,
+      MDBModalTitle,
+      MDBRow,
+      MDBTable,
+      PageWrapper
+    },
+    setup() {
+      const { currentUser } = getUser();
+      const firstName = ref('');
+      const lastName = ref('');
+      const address = ref('');
+      const email = ref('');
+      const phoneNumber = ref('');
+      const experimentalData = ref([]);
+      const deleteConfirmationModal = ref(false);
+      const currentDocId = ref('');
+      const currentEntity = ref([]);
+      const router = useRouter();
+      const cardsVisible = ref(false);
+      const showCardContainer = ref(false);
+      const experimentalDataModal = ref(false);
+      const tableHeaders = reactive([
+        'Gene Mutation',
+        'ledv',
+        'redv',
+        'lesv',
+        'resv',
+        'lvef',
+        'rvef',
+        'lvmass',
+        'lsv',
+        'rsv',
+        'Gender',
+        'Fibrosis',
+        'Age at MRI',
+        'Apical HCM',
+        'Sudden Cardiac Death',
+        'Hypertension',
+        'Diabetes',
+        'Myectomy',
+        'Date Created',
+        'Actions'
+      ]);
 
-    const userIdentity = async () => {
-      const docRef = doc(await store.database, "users", currentUser.value.uid);
-      getDoc(docRef).then((docSnap) => {
-        if (!docSnap.exists()) return;
-
-        firstName.value = docSnap.data().firstName;
-        lastName.value = docSnap.data().lastName;
-        address.value = docSnap.data().address;
-        email.value = docSnap.data().email;
-        phoneNumber.value = docSnap.data().phone;
-
-        if (process.env.DEVELOPMENT)
-          console.log("Document data:", docSnap.data());
-      });
-    };
-
-    const updateUser = async () => {
-      const currentUserRef = doc(
-        await store.database,
-        "users",
-        currentUser.value.uid
-      );
-
-      // Set the "capital" field of the city 'DC'
-      await updateDoc(currentUserRef, {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        address: address.value,
-        email: email.value,
-        phone: phoneNumber.value,
+      onMounted(() => {
+        userIdentity();
+        getExperimentalDataByUserId();
       });
 
-      //set user display name
-      try {
-        updateProfile(auth.currentUser, {
-          displayName: firstName.value,
-        })
-          .then(alert("User profile updated."))
-          .then(() => {
-            router.push("/");
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    };
+      const userIdentity = async () => {
+        const docRef = doc(await store.database, 'users', currentUser.value.uid);
+        getDoc(docRef).then((docSnap) => {
+          if (!docSnap.exists()) return;
 
-    const getExperimentalDataByUserId = async () => {
-      const firestoreQuery = query(
-        collection(await store.database, "hcmData"),
-        where("userId", "==", currentUser.value.uid)
-      );
+          firstName.value = docSnap.data().firstName;
+          lastName.value = docSnap.data().lastName;
+          address.value = docSnap.data().address;
+          email.value = docSnap.data().email;
+          phoneNumber.value = docSnap.data().phone;
 
-      const querySnapshot = await getDocs(firestoreQuery);
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        experimentalData.value.push({ documentId: doc.id, ...doc.data() });
-        if (process.env.DEVELOPMENT) console.log(doc.id, " => ", doc.data());
+          if (process.env.DEVELOPMENT)
+            console.log('Document data:', docSnap.data());
+        });
+      };
+
+      const updateUser = async () => {
+        const currentUserRef = doc(
+          await store.database,
+          'users',
+          currentUser.value.uid
+        );
+
+        // Set the "capital" field of the city 'DC'
+        await updateDoc(currentUserRef, {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          address: address.value,
+          email: email.value,
+          phone: phoneNumber.value
+        });
+
+        //set user display name
+        try {
+          updateProfile(auth.currentUser, {
+            displayName: firstName.value
+          })
+            .then(alert('User profile updated.'))
+            .then(() => {
+              router.push('/');
+            });
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      const getExperimentalDataByUserId = async () => {
+        const firestoreQuery = query(
+          collection(await store.database, 'hcmData'),
+          where('userId', '==', currentUser.value.uid)
+        );
+
+        const querySnapshot = await getDocs(firestoreQuery);
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          experimentalData.value.push({ documentId: doc.id, ...doc.data() });
+          if (process.env.DEVELOPMENT) console.log(doc.id, ' => ', doc.data());
+        });
+      };
+
+      const deleteExperimentalDataDoc = async (docId) => {
+        //Update the deleted timestamp
+        const docRef = doc(await store.database, 'hcmData', docId);
+
+        // Update the timestamp field with the value from the server
+        await updateDoc(docRef, {
+          deletedAt: serverTimestamp()
+        });
+
+        //Delete the document from the database
+        await deleteDoc(doc(await store.database, 'hcmData', docId));
+        await (alert('Data has been successfully deleted.'), router.push('/'));
+      };
+
+      const toggleModal = (docId) => {
+        deleteConfirmationModal.value = !deleteConfirmationModal.value;
+        currentDocId.value = docId;
+      };
+
+      const showCards = () => {
+        showCardContainer.value = true;
+        cardsVisible.value = !cardsVisible.value;
+      };
+
+      const toggleExperimentalDataModal = (entity) => {
+        experimentalDataModal.value = !experimentalDataModal.value;
+        delete entity.deletedAt;
+        delete entity.documentId;
+        delete entity.userId;
+        delete entity.createdByUser;
+
+        currentEntity.value = entity;
+        if (process.env.DEVELOPMENT) console.log(currentEntity.value);
+      };
+
+      watchEffect(() => {
+        if (!currentUser.value) router.push('/login');
       });
-    };
 
-    const deleteExperimentalDataDoc = async (docId) => {
-      //Update the deleted timestamp
-      const docRef = doc(await store.database, "hcmData", docId);
+      const dateTime = (value) =>
+        new Date(value).toLocaleDateString('en-GB', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        });
 
-      // Update the timestamp field with the value from the server
-      await updateDoc(docRef, {
-        deletedAt: serverTimestamp(),
-      });
-
-      //Delete the document from the database
-      await deleteDoc(doc(await store.database, "hcmData", docId));
-      await (alert("Data has been successfully deleted."), router.push("/"));
-    };
-
-    const toggleModal = (docId) => {
-      deleteConfirmationModal.value = !deleteConfirmationModal.value;
-      currentDocId.value = docId;
-    };
-
-    const showCards = () => {
-      showCardContainer.value = true;
-      cardsVisible.value = !cardsVisible.value;
-    };
-
-    const toggleExperimentalDataModal = (entity) => {
-      experimentalDataModal.value = !experimentalDataModal.value;
-      delete entity.deletedAt;
-      delete entity.documentId;
-      delete entity.userId;
-      delete entity.createdByUser;
-
-      currentEntity.value = entity;
-      if (process.env.DEVELOPMENT) console.log(currentEntity.value);
-    };
-
-    watchEffect(() => {
-      if (!currentUser.value) router.push("/login");
-    });
-
-    const dateTime = (value) =>
-      new Date(value).toLocaleDateString("en-GB", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-
-    return {
-      address,
-      cardsVisible,
-      currentDocId,
-      currentEntity,
-      dateTime,
-      deleteConfirmationModal,
-      deleteExperimentalDataDoc,
-      email,
-      experimentalData,
-      experimentalDataModal,
-      firstName,
-      getExperimentalDataByUserId,
-      lastName,
-      mapKeyToWords,
-      phoneNumber,
-      showCardContainer,
-      showCards,
-      tableHeaders,
-      toggleExperimentalDataModal,
-      toggleModal,
-      updateUser,
-      userIdentity,
-    };
-  },
-};
+      return { address, cardsVisible, currentDocId, currentEntity, dateTime, deleteConfirmationModal,
+               deleteExperimentalDataDoc, email, experimentalData, experimentalDataModal, firstName,
+               getExperimentalDataByUserId, lastName, mapKeyToWords, phoneNumber, showCardContainer,
+               showCards, tableHeaders, toggleExperimentalDataModal, toggleModal, updateUser, userIdentity };
+    }
+  };
 </script>
 
 <style lang="scss" module scoped>
-@import "../../assets/styles/Authentication.scss";
-@import "./UserProfile.module.scss";
+  @import "../../assets/styles/Authentication.scss";
+  @import "./UserProfile.module.scss";
 </style>
